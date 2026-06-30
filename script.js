f26f039adc25579650409d9a6ff93ac491bb5f10
ahsanhabibdm <ahsanhabib.dm1@gmail.com>
2026-06-30 21:57:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ behavior:"smooth"
 });
 });
 });
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+    link.addEventListener("click",function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior:"smooth"
+        });
+    });
+});
