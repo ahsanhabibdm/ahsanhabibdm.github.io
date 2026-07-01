8210adafc2c51e764b4d1c78d267fd204cbb7264
ahsanhabibdm <ahsanhabib.dm1@gmail.com>
2026-07-01 11:22:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ document.querySelectorAll('a[href^="#"]').forEach(link=>{
         });
     });
 });
+function openPricing(evt, platformId) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active-content");
+    }
+    tablinks = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(platformId).classList.add("active-content");
+    evt.currentTarget.classList.add("active");
+}
